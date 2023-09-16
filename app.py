764e6937e0d459ec74be26f2b6d6946088d50b68
Gokhan Cag <gokhan.cag@radity.com>
2023-09-16 15:30:05 +0300

    added app files

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def main():
     return "Selam!"
 
-@app.route('/how are you')
+@app.route('/nasilsin')
 def hello():
-    return 'I am good, how about you?'
+    return 'iyiyim, sen nasilsin?'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
